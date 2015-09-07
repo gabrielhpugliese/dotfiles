08190c703b1614e13fdb2b86d8d4a5d949f18b27
Gabriel Pugliese <gabrielhpugliese@gmail.com>
2015-09-07 18:06:35 -0300

    Fix install and vim scripts

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-add-apt-repository ppa:chris-lea/node.js
-add-apt-repository ppa:pi-rho/dev
-apt-get --yes update
-apt-get --yes install gnome git vim-gtk tmux nodejs python-pip python-software-properties ctags python-dev htop libjpeg-dev libgif-dev curl xsel
+sudo apt-get --yes update
+sudo apt-get --yes install git vim-gtk tmux python-pip python-software-properties ctags python-dev htop libjpeg-dev libgif-dev curl xsel cmake
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
-gem install tmuxinator
-pip install virtualenvwrapper
+sudo gem install tmuxinator
+sudo pip install virtualenvwrapper
 
 bash powerline.sh
 bash vim.sh
--- vim.sh
+++ vim.sh
@@ -2,9 +2,12 @@
 
 cd /tmp
 git clone https://github.com/gabrielhpugliese/dotfiles.git
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
 cd dotfiles
 cp -r vim ~/.vim
 ln -s ~/.vim/vimrc ~/.vimrc
 mkdir -p ~/.vimfiles/backup/
 vim +BundleInstall +qall
+cd /tmp
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
+cd ~/.vim/bundle/vundle
+./install.py
